02889584de64d0cf8596df280e42e1ff18903e95
shlokD12 <162155087+shlokD12@users.noreply.github.com>
2026-01-22 04:26:13 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,56 @@
+import express from "express";
+import crypto from "crypto";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+const PAYU_KEY = process.env.PAYU_KEY;
+const PAYU_SALT = process.env.PAYU_SALT;
+
+// PayU TEST URL (safe for demo)
+const PAYU_URL = "https://test.payu.in/_payment";
+
+app.get("/pay", (req, res) => {
+  const { amount, firstname, email } = req.query;
+
+  if (!amount || !firstname || !email) {
+    return res.send("Missing parameters");
+  }
+
+  const txnid = "TXN" + Date.now();
+  const productinfo = "Demo Financial Course";
+
+  const surl = "https://www.bkcashmanagement.com/payment-success";
+  const furl = "https://www.bkcashmanagement.com/payment-failed";
+
+  const hashString =
+    `${PAYU_KEY}|${txnid}|${amount}|${productinfo}|${firstname}|${email}` +
+    `|||||||||||${PAYU_SALT}`;
+
+  const hash = crypto
+    .createHash("sha512")
+    .update(hashString)
+    .digest("hex");
+
+  res.send(`
+    <html>
+      <body onload="document.forms[0].submit()">
+        <form method="post" action="${PAYU_URL}">
+          <input type="hidden" name="key" value="${PAYU_KEY}" />
+          <input type="hidden" name="txnid" value="${txnid}" />
+          <input type="hidden" name="amount" value="${amount}" />
+          <input type="hidden" name="productinfo" value="${productinfo}" />
+          <input type="hidden" name="firstname" value="${firstname}" />
+          <input type="hidden" name="email" value="${email}" />
+          <input type="hidden" name="surl" value="${surl}" />
+          <input type="hidden" name="furl" value="${furl}" />
+          <input type="hidden" name="hash" value="${hash}" />
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("PayU demo server running");
+});
